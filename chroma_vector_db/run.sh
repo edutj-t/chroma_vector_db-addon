6935e0f094a7348bfe8a31de61a4997a0c88775a
edutj-t <73550585+edutj-t@users.noreply.github.com>
2025-02-21 16:55:25 +0100

    Update run.sh

--- chroma_vector_db/run.sh
+++ chroma_vector_db/run.sh
@@ -4,9 +4,8 @@ set -e
 # -----------------------------------------------------------------------------
 # Chroma Vector DB add-on service script
 #
-# This script is executed automatically by s6-overlay (/init running as PID 1)
+# This script is executed automatically by s6-overlay (/init running as PID 1).
 # It uses Bashio to retrieve configuration and log messages.
-#
 # Do NOT invoke any s6 commands here.
 # -----------------------------------------------------------------------------
 
@@ -18,3 +17,4 @@ bashio::log.info "Chroma is pre-installed. Launching on port: ${PORT}"
 
 # Launch the chromadb server
 exec python3 -m chromadb --port "${PORT}"
+
