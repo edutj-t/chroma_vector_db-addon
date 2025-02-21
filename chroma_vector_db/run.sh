d746c9e1a1fb6d316481338f63560aed1f4a43c3
edutj-t <73550585+edutj-t@users.noreply.github.com>
2025-02-21 14:56:18 +0100

    Update run.sh

--- chroma_vector_db/run.sh
+++ chroma_vector_db/run.sh
@@ -15,7 +15,7 @@ if [ "${CHROMA_VERSION}" = "latest" ]; then
 else
     pip install --no-cache-dir --upgrade chromadb=="${CHROMA_VERSION}"
 fi
-
+export S6_BEHAVIOUR_IF_STAGE2_FAILS=2
 # Start ChromaDB properly as PID 1
 bashio::log.info "Starting Chroma..."
 exec chromadb run --host 0.0.0.0 --port "${PORT}"
